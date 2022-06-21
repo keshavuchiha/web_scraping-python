30422052880ce6944700c09d0cab9a7a78e6c676
keshav goel <keshavgoel_2k19co186@dtu.ac.in>
2022-06-21 21:19:13 +0530

    urllib

--- first.py
+++ first.py
@@ -1,7 +1,16 @@
-import requests
+# import requests
+from email.policy import HTTP
+from urllib.error import HTTPError, URLError
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 url='http://www.pythonscraping.com/pages/page1.html'
-req=requests.get(url)
+req=urlopen(url)
 # print(req.text)
-bs = BeautifulSoup(req.text, 'html.parser')
-print(bs.h1)
+try:
+    bs = BeautifulSoup(req.read(), 'lxml')
+except HTTPError as e:
+    print(e)
+except URLError as e:
+    print('the server not found')
+else:
+    print(bs.h1)
